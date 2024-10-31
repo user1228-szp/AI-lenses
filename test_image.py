c67dc10012d5158dd25ddc7f775cdaf0e3f97bd3
Sebastian Eugenio <82603912+user1228-szp@users.noreply.github.com>
2024-10-31 20:36:29 -0300

    Update test_image.py

--- test_image.py
+++ test_image.py
@@ -5,30 +5,21 @@ import numpy as np
 from ultralytics import YOLO
 
 def process_image_2(image_path, model_path):
-    # Cargar el modelo YOLOv8
     model = YOLO(model_path)
-
-    # Cargar la imagen
+    
     image = cv2.imread(image_path)
 
     if image is None:
         print(f"Error: no se pudo cargar la imagen en {image_path}")
         exit()
 
-    # Realizar la predicción
     results = model(image)
-
-    # Filtrar detecciones con confianza mayor a 0.5
     filtered_indices = np.where(results[0].boxes.conf.cpu().numpy() > 0.5)[0]
-
-    # Obtener las cajas correspondientes a las detecciones filtradas
     boxes = results[0].boxes.xyxy.cpu().numpy()[filtered_indices].astype(int)
 
-    # Dibujar las detecciones en la imagen
     for (xmin, ymin, xmax, ymax) in boxes:
         cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color=(0, 255, 0), thickness=2)
-
-    # Mostrar la imagen usando matplotlib
+        
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.axis('off')
     plt.show()
